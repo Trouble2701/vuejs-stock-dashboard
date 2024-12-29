47490ff53726048a5d9d2a4635c8462e70d7cd21
Sven Friedrich <128075628+Trouble2701@users.noreply.github.com>
2024-12-29 13:35:35 +0100

    first component created

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+  <div>
+    <BaseCard>
+      <h1>Überschrift</h1>
+      <p>Das ist ein Beispieltext in der Karte.</p>
+    </BaseCard>
+  </div>
+  </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import BaseCard from './components/BaseCard.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   BaseCard
   }
 }
 </script>
 
 <style>
+
+body{
+  margin: 0;
+  padding: 100px;
+  min-height: 100vh;
+  background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="base-card">
+      <slot></slot>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'BaseCard',
+  };
+  </script>
+  
+  <style scoped>
+  .base-card {
+    background-color: rgba(1, 31, 53, 1);
+    padding: 20px 32px;
+    border-radius: 16px;
+  }
+  </style>
+  
