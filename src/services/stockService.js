5b9d2e2dbbd81dce9ad04dae778c05bc6828e667
Sven Friedrich <128075628+Trouble2701@users.noreply.github.com>
2024-12-31 13:23:05 +0100

    data sort

--- src/services/stockService.js
+++ src/services/stockService.js
@@ -1,5 +1,22 @@
 import axios from 'axios';
 
+const order = [
+  'Mar 21',
+  'Jun 21',
+  'Sep 21',
+  'Dec 21',
+  'Mar 22',
+  'Jun 22',
+  'Sep 22',
+  'Dec 22',
+  'Mar 23',
+  '""',
+  '3 Aug 23',
+  '2 Nov 23',
+  '1 Feb 24',
+  '2 Mai 24'
+];
+
 class StockService {
   constructor() {
     this.apiClient = axios.create({
@@ -16,9 +33,10 @@ class StockService {
       return [];
     }
   }
-   // Neue Methode `getRevenue`
-   async getRevenue(sheetName) {
-    return this.fetchData(sheetName);
+
+  async getRevenue(sheetName) {
+    const data = await this.fetchData(sheetName)
+    return order.map(key => data[3][key]);
   }
 }
 
